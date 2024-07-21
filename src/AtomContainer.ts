de7efbcc64ce5f2bb33ea50d89935713c14902af
MasatoMakino <makino.masato.g@gmail.com>
2024-07-21 13:06:32 +0900

    feat: Refactor AtomContainer class to support JSON serialization

--- src/AtomContainer.ts
+++ src/AtomContainer.ts
@@ -1,25 +1,62 @@
 import EventEmitter from "eventemitter3";
-import { Atom, AtomEvents } from "./Atom";
+import { Atom, AtomEventArgs, AtomEvents } from "./Atom";
 
-export class AtomContainer extends EventEmitter<AtomEvents<undefined>> {
+export class AtomContainer<T = any> extends EventEmitter<AtomEvents<unknown>> {
   constructor() {
     super();
+
+    for (const value of Object.values(this)) {
+      if (value instanceof Atom || value instanceof AtomContainer) {
+        value.on("beforeChange", (arg: AtomEventArgs<any>) => {
+          this.emit("beforeChange", arg);
+        });
+        value.on("change", (arg: AtomEventArgs<any>) => {
+          this.emit("change", arg);
+        });
+        value.on("addHistory", () => {
+          this.emit("addHistory");
+        });
+      }
+    }
   }
 
+  /**
+   * このクラスに保持されているAtomおよびAtomContainerの値をオブジェクトにコピーする。
+   * @returns T
+   */
+  toObject(): T {
+    const obj = {};
+    for (const [key, value] of Object.entries(this)) {
+      if (value instanceof AtomContainer) {
+        obj[key] = value.toObject();
+      } else if (value instanceof Atom) {
+        obj[key] = value.value;
+      }
+    }
+    return obj as T;
+  }
   /**
    * このクラスに保持されているAtomおよびAtomContainerの値をJSON文字列に変換する。
    * @returns string
    */
   toJson(): string {
-    const json = {};
-    for (const [key, value] of Object.entries(this)) {
-      if (value instanceof AtomContainer) {
-        json[key] = value.toJson();
-      } else if (value instanceof Atom) {
-        json[key] = value.value;
+    const obj = this.toObject();
+    return JSON.stringify(obj);
+  }
+
+  /**
+   * ObjectからAtomおよびAtomContainerの値を復元する。
+   * @param json
+   */
+  fromObject(obj: T): void {
+    for (const [key, value] of Object.entries(obj as {})) {
+      if (this[key] instanceof AtomContainer) {
+        this[key].fromObject(value);
+      }
+      if (this[key] instanceof Atom) {
+        this[key].value = value;
       }
     }
-    return JSON.stringify(json);
   }
 
   /**
@@ -28,13 +65,6 @@ export class AtomContainer extends EventEmitter<AtomEvents<undefined>> {
    */
   fromJson(json: string): void {
     const jsonObj = JSON.parse(json);
-    for (const [key, value] of Object.entries(jsonObj)) {
-      if (this[key] instanceof AtomContainer) {
-        this[key].fromJson(JSON.stringify(value));
-      }
-      if (this[key] instanceof Atom) {
-        this[key].value = value;
-      }
-    }
+    this.fromObject(jsonObj);
   }
 }
